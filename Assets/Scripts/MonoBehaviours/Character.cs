280d32ba8ab378f7aafeaa260ded8728673755c9
Vo Huu Nam Truong <namtruong0900@gmail.com>
2022-09-24 20:44:45 +0700

    Added Health Bar
    Create HealthBarObject
    Create HealthBar Prefab
    Add HealthBar Prefab to PlayerObject
    
    Updated Character Script
    Updated Player Script

--- Assets/Scripts/MonoBehaviours/Character.cs
+++ Assets/Scripts/MonoBehaviours/Character.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public abstract class Character : MonoBehaviour
 {
-    public int hitPoints;
-    public int maxHitPoints;
+    public HitPoints hitPoints;
+    public float maxHitPoints;
+
+    public float startingHitPoints;
 }
--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Player : Character
 {
+    public HealthBar healthBarPrefab;
+
+    HealthBar healthBar;
+
+    void Start()
+    {
+        hitPoints.value = startingHitPoints;
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.character = this;
+    }
+
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("CanBePickedUp"))
@@ -12,26 +24,40 @@ public class Player : Character
 
             if (hitObject != null)
             {
-                print("Hit: " + hitObject.objectName);
+                //print("Hit: " + hitObject.objectName);
+
+                bool shouldDisappear = false;
 
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
+
+                        shouldDisappear = true;
                         break;
 
                     case Item.ItemType.HEALTH:
-                        AdjustHitPoints(hitObject.quantity);
+
+
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
                         break;
                 }
 
-                collision.gameObject.SetActive(false);
+                if (shouldDisappear)
+                    collision.gameObject.SetActive(false);
             }
         }
     }
 
-    public void AdjustHitPoints(int amount)
+    public bool AdjustHitPoints(int amount)
     {
-        hitPoints = hitPoints + amount;
-        print("Adjusted HitPoints by: " + amount + ". New value: " + hitPoints);
+        if (hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+            print("Adjusted HitPoints by: " + amount + ". New value: " + hitPoints.value);
+            return true;
+        }
+        return false;
     }
 }
