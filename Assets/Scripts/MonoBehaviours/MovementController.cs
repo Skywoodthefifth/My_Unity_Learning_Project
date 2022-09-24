cafa2ade1c5362cd73e0dc90ed6007f965dc6bde
Vo Huu Nam Truong <namtruong0900@gmail.com>
2022-09-24 14:45:44 +0700

    Init_2

---
+++ Assets/Scripts/MonoBehaviours/MovementController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    Vector2 movement = new Vector2();
+
+    Animator animator;
+    string animationState = "AnimationState";
+
+    enum CharStates {
+        Idle,
+        Walk,
+        Attack,
+        Die,
+    }
+
+    Rigidbody2D rb2D;
+
+    private SpriteRenderer mySpriteRenderer;
+
+    bool isFacingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        isFacingRight = true;
+
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateState();
+        MovementFlip();
+    }
+
+    void FixedUpdate() {
+        
+        MoveCharacter();
+
+    }
+
+    private void MoveCharacter() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+        rb2D.velocity = movement * movementSpeed;
+    }
+
+    private void MovementFlip() {
+        //Flip the sprite of when moving left or right
+        if(movement.x < 0f) {
+
+            isFacingRight = true;
+
+            mySpriteRenderer.flipX = isFacingRight;
+
+        } else if(movement.x > 0f) { 
+
+            isFacingRight = false;
+
+            mySpriteRenderer.flipX = isFacingRight;
+        }
+    }
+
+    private void UpdateState() {
+        
+        if(movement.x != 0 || movement.y != 0) 
+        {
+            animator.SetInteger(animationState, (int) CharStates.Walk);
+        } 
+        else if (movement.x == 0 && movement.y == 0)
+        {
+            animator.SetInteger(animationState, (int) CharStates.Idle);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
+            if (hitObject != null)
+            {
+                print("Hit: " + hitObject.objectName);
+
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        break;
+
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                }
+
+                collision.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void AdjustHitPoints(int amount)
+    {
+        hitPoints = hitPoints + amount;
+        print("Adjusted HitPoints by: " + amount + ". New value: " + hitPoints);
+    }
+}
