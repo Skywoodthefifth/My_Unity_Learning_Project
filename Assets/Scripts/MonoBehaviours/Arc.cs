5105bd479f1405148a77a1986b7b767862740054
Vo Huu Nam Truong <namtruong0900@gmail.com>
2022-09-26 09:28:26 +0700

    Added Ammo and Enemy Wander

---
+++ Assets/Scripts/MonoBehaviours/Arc.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arc : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator TravelArc(Vector3 destination, float duration)
+    {
+        var startPosition = transform.position;
+        var percentComplete = 0.0f;
+        while (percentComplete < 1.0f)
+        {
+            percentComplete += Time.deltaTime / duration;
+            transform.position = Vector3.Lerp(startPosition, destination, percentComplete);
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+}
