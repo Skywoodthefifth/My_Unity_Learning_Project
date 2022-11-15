7a7a0c13f4f7a9eb585aed46000e634291a44698
Nguyen Cong Anh <ncanh2806@gmail.com>
2022-11-15 13:08:31 +0700

    Fix some bug

---
+++ Assets/Scripts/MonoBehaviours/LvLUpMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvLUpMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+
+    PauseManager pauseManager;
+
+
+    private void Awake()
+    {
+        pauseManager = GetComponent<PauseManager>();
+        panel.SetActive(false);
+    }
+    public void OpenPanel()
+    {
+        pauseManager.PauseGame();
+        panel.SetActive(true);
+    }
+    public void ClosePanel()
+    {
+
+        pauseManager.UnPauseGame();
+        panel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (panel.activeInHierarchy == false)
+            {
+                OpenPanel();
+            }
+            else
+            {
+                ClosePanel();
+            }
+        }
+    }
+
+
+}
